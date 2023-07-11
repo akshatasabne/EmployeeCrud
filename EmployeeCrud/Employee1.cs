2e17e291e206afdea08f64cd18f83585ae5c069d
Akshata <Akshata@LAPTOP-4CVBJOT7>
2023-07-11 10:40:56 +0530

    Add project files.

---
+++ EmployeeCrud/Employee1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeCrud
+{
+    public class Employee1
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+
+    public class EmployeeCrud
+    {
+        private List<Employee1> employees;
+
+        public EmployeeCrud()
+        {
+            new List<Employee1>();
+        }
+        public void AddEmployee(Employee1 employee)
+        {
+            employees.Add(employee);
+        }
+        public void UpdateEmployee(Employee1 employee)
+        {
+            foreach (Employee1 employee1 in employees)
+            {
+                if (employee1.Id == employee.Id)
+                {
+                    employee1.Name = employee.Name;
+                    employee1.Salary = employee.Salary;
+
+                }
+            }
+        }
+        public void DeleteEmployee(int Id)
+        {
+            foreach (Employee1 employee1 in employees)
+            {
+                if (employee1.Id == Id)
+                {
+                    employees.Remove(employee1);
+                }
+            }
+        }
+        public List<Employee1> GetEmployee()
+        {
+            return employees;
+        }
+    }
+}
---
+++ EmployeeCrud/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeCrud
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EmployeeCrud crud = new EmployeeCrud();
+            int option = 0;
+            do
+            {
+                Console.WriteLine("1.Add Employee Details");
+                Console.WriteLine("2.Update Employee Details");
+                Console.WriteLine("3.Delete Employee Details");
+                Console.WriteLine("1.Display all Employee Details");
+                int op = Convert.ToInt32(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1:
+                        Employee1 e1 = new Employee1();
+                        Console.WriteLine("Enter Employee Id");
+                        e1.Id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Employee Name");
+                        e1.Name = Console.ReadLine();
+                        Console.WriteLine("Enter Employee Salary");
+                        e1.Salary = Convert.ToInt32(Console.ReadLine());
+
+                        crud.AddEmployee(e1);
+                        break;
+
+                    case 2:
+                        Employee1 e2 = new Employee1();
+                        Console.WriteLine("Enter Employee Id");
+                        e2.Id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Employee Name");
+                        e2.Name = Console.ReadLine();
+                        Console.WriteLine("Enter Employee Salary");
+                        e2.Salary = Convert.ToInt32(Console.ReadLine()); Console.WriteLine("Enter Employee Id");
+
+                        crud.UpdateEmployee(e2);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Enter Employee Id");
+                        int Id = Convert.ToInt32(Console.ReadLine());
+                        crud.DeleteEmployee(Id);
+                        break;
+
+                    case 4:
+                        List<Employee1> list = crud.GetEmployee();
+                        foreach (Employee1 e in list)
+                        {
+                            Console.WriteLine($"{e.Id}-{e.Name}-{e.Salary}");
+                        }
+                        break;
+                }
+                Console.WriteLine("Enter 1 to Continue");
+                option = Convert.ToInt32(Console.ReadLine());
+
+            } while (option == 1);
+        }
+    }
+}
